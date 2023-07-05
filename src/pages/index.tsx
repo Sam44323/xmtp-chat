44ab4bd32972b2df8e961ab0f52e9934da21813f
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-06 02:34:05 +0530

    add: added options for signer-connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,50 @@
+import React from "react";
 import styles from "@/styles/Home.module.css";
 import { Button } from "reactstrap";
+import { useWalletLogin, useActiveProfile } from "@lens-protocol/react-web";
+import { useAccount, useConnect, useDisconnect, useSigner } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 export default function Home() {
+  const {
+    execute: loginLens,
+    error: loginError,
+    isPending: isLoginPending,
+  } = useWalletLogin();
+
+  const { data } = useActiveProfile();
+  const { data: signerData } = useSigner();
+
+  const { isConnected } = useAccount();
+  const { disconnectAsync } = useDisconnect();
+
+  const { connectAsync } = useConnect({
+    connector: new InjectedConnector(),
+  });
+
+  const onLoginWithMetamaskClick = async () => {
+    if (isConnected) {
+      await disconnectAsync();
+      return;
+    }
+
+    const { connector } = await connectAsync();
+
+    if (connector instanceof InjectedConnector) {
+      const signer = await connector.getSigner();
+      console.log(signer);
+      const response = await loginLens(signer);
+      console.log(response);
+    }
+  };
+
+  React.useEffect(() => {
+    console.log(data);
+    if (data) {
+      console.log(signerData);
+    }
+  }, [data]);
+
   return (
     <div>
       <section
@@ -13,7 +56,9 @@ export default function Home() {
       "
       >
         <h1 className={styles.title}>XMTP Chat</h1>
-        <Button color="primary">Connect Wallet</Button>
+        <Button color="primary" onClick={onLoginWithMetamaskClick}>
+          Connect Wallet
+        </Button>
       </section>
     </div>
   );
